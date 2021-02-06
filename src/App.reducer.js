3f38bcee22634ca0ec44ae1f8fd352f2039a721a
song <johyesong8686@gmail.com>
2021-02-06 19:03:41 +0900

    feat: 왕기초 ,초보 기능 App.reducer에 추가

--- src/App.reducer.js
+++ src/App.reducer.js
@@ -1,8 +1,8 @@
-import React, { useReducer, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback, useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-
+import TodoEdit from './components/TodoEdit';
 function createBulkTodos() {
   const array = [];
   for (let i = 1; i <= 2500; i++) {
@@ -31,7 +31,7 @@ function todoReducer(todos, action) {
     case 'EDIT': // 수정
       // { type: 'REMOVE', id: 1 }
       return todos.map((todo) =>
-        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+        todo.id === action.id ? { ...todo, text: action.editText } : todo,
       );
     default:
       return todos;
@@ -40,6 +40,8 @@ function todoReducer(todos, action) {
 
 const App = () => {
   const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  const [editMode, setEditMode] = useState(false);
+  const [initTodo, setInitTodo] = useState('');
 
   // 고유 값으로 사용 될 id
   // ref 를 사용하여 변수 담기
@@ -63,10 +65,33 @@ const App = () => {
     dispatch({ type: 'TOGGLE', id });
   }, []);
 
+  const onEdit = useCallback((id, editText) => {
+    dispatch({ type: 'EDIT', id, editText });
+  }, []);
+
+  const onClickEdit = useCallback((todoOne) => {
+    setEditMode((pre) => !pre);
+    setInitTodo(todoOne);
+  }, []);
+
   return (
     <TodoTemplate>
-      <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      {editMode ? (
+        <TodoEdit
+          onEdit={onEdit}
+          setEditMode={setEditMode}
+          initTodo={initTodo}
+        />
+      ) : (
+        <TodoInsert onInsert={onInsert} />
+      )}
+
+      <TodoList
+        todos={todos}
+        onRemove={onRemove}
+        onClickEdit={onClickEdit}
+        onToggle={onToggle}
+      />
     </TodoTemplate>
   );
 };
